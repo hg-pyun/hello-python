b397e8962bef3972bbad94d9a6e76757d6042057
haegul <phg2491@gmail.com>
2017-10-09 16:10:32 +0900

    숫자형

---
+++ 02-1 숫자형.py
@@ -0,0 +1,21 @@
+# Number
+integerNumber = 100
+floatNumber = 1.2
+print('number', integerNumber, floatNumber)
+
+# 8진수, 16진수
+octal = 0o177
+hex = 0xABC
+print('X 진수', octal, hex)
+
+# 사칙연산
+a = 3
+b = 4
+print(a + b, a * b, a / b)
+
+# 거듭제곱
+print(a ** b)
+
+# 소수점 아랫자리 버림 //
+print(7 / 4, 7 // 4)   # -1.75 1
+print(-7 / 4, -7 // 4) # -1.75 -2
